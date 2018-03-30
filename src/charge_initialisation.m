b98c2265f4fc76ce77e9f8e52d7fb89f04d4c5f3
Tirma <thomas-poletti@outlook.com>
2018-03-30 15:28:46 +0200

    rho init

--- src/charge_initialisation.m
+++ src/charge_initialisation.m
@@ -2,10 +2,14 @@ function rho_init = charge_initialisation(X,Nx,Wdep_max,tox,Na)
 
 physical_constants
 
-
-
 %%%%---A COMPLETER---%%%%
 
+rho_init = 1:1:Nx;
+rho_init = 0*rho_init;
+
+% Condition de Dirichlet à gauche (masse à doite)
+Vg = 0;
+rho_init(1) = Vg;
+rho_init(Nx) = 0;
 
 end
-
