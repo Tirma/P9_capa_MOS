5bc9cae0abdbf660c6a370e4b367cb730bc553e2
miquelr <miquelr@d7010m15909.phelma.grenoble-inp.fr>
2018-03-30 15:24:26 +0200

    Lp

--- src/Laplacien_Poisson.m
+++ src/Laplacien_Poisson.m
@@ -1,10 +1,25 @@
 function Lp = Laplacien_Poisson(Eps,X)
 
-% Calcul des diagonales supérieurs (Ap) et inférieur (Bp)
+% Calcul des diagonales supï¿½rieurs (Ap) et infï¿½rieur (Bp)
 
 % Question preliminaire : combien de point pour la matrice Laplacienne ?
 
 %%%%---A COMPLETER---%%%% 
+% CAL de Dirichlet
+N = length(X);
+Lp = sparse(N,N);
 
+%Dirichlet
+Lp(1, 1) = 1;
+Lp(N, N) = 1;
 
+% Points interieurs du maillage
+for p=2:N-1
+    G = (Eps(p-1) + Eps(p))/((X(p+1)-X(p-1))*(X(p)-x(p-1)));
+    D = (Eps(p+1) + Eps(p))/((X(p+1)-X(p-1))*(X(p+1)-x(p)));
+    C = D - G;
+    
+    Lp(p,p-1) = G;
+    Lp(p,p) = C;
+    Lp(p,p+1) = D;
 end
